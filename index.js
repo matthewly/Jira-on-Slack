e4e57504fa6c894ebbbc39dc5049c1e39c18962c
Matthew Ly <matthew.ly@mly-01.workdayinternal.com>
2016-12-01 17:56:40 -0800

    integrate slack slash command

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
-// Import express and request modules
+
 var express = require('express');
 var request = require('request');
+var bodyParser = require('body-parser');
 
-// Store our app's ID and Secret. These we got from Step 1.
-// For this tutorial, we'll keep your API credentials right here. But for an actual app, you'll want to  store them securely in environment variables.
 var clientId = '110549175985.110544469152';
 var clientSecret = 'a5b98c4f3734690601df13231d913d36';
 
-// Instantiates Express and assigns our app variable to it
 var app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
-// Again, we define a port we want to listen to
 const PORT=4350;
 
-// Lets start our server
+
 app.listen(PORT, function () {
-    //Callback triggered when server is successfully listening. Hurray!
     console.log("Jira4Ever app listening on port " + PORT);
 });
 
-
-// This route handles GET requests to our root address and responds with the same "Ngrok is working message" we used before
 app.get('/', function(req, res) {
     res.send('Jira4Ever is working! Path Hit: ' + req.url);
 });
 
-// This route handles get request to a /oauth endpoint. We'll use this endpoint for handling the logic of the Slack oAuth process behind our app.
 app.get('/oauth', function(req, res) {
     // When a user authorizes an app, a code query parameter is passed on the oAuth endpoint. If that code is not there, we respond with an error message
     if (!req.query.code) {
@@ -40,7 +35,7 @@ app.get('/oauth', function(req, res) {
         request({
             url: 'https://slack.com/api/oauth.access', //URL to hit
             qs: {code: req.query.code, client_id: clientId, client_secret: clientSecret}, //Query string data
-            method: 'GET', //Specify the method
+            method: 'GET',
 
         }, function (error, response, body) {
             if (error) {
@@ -52,8 +47,8 @@ app.get('/oauth', function(req, res) {
         })
     }
 
-    // Route the endpoint that our slash command will point to and send back a simple response to indicate that ngrok is working
     app.post('/command', function(req, res) {
-        res.send('mike g the g!!!');
+        console.log(req.body.text);
+        res.send("mike g the g!!!");
     });
 });
